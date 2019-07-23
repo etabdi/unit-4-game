560a232986935d3f21b31f208e1dbfd3d95a9663
Abdu <51891078+etabdi@users.noreply.github.com>
2019-07-23 15:32:00 -0500

    spelling "number' edited

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -162,14 +162,14 @@ $(function(){
   })
   
   $('.rab').click(function(){
-    alert("You will be given a random number at the start of the game.  There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score.You win the game by matching your total score to the ramdomnumber, you lose the game if your total score goes above the random number.The value of each crystal is hidden from you until you click on it. Each time when the game starts,the game will change the values of each crystal.", function(){
+    alert("You will be given a random number at the start of the game.  There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score.You win the game by matching your total score to the ramdom number, you lose the game if your total score goes above the random number.The value of each crystal is hidden from you until you click on it. Each time when the game starts,the game will change the values of each crystal.", function(){
       console.log("Callback executed");
     })
   });
   
   // our custom alert box
   setTimeout(function(){
-    alert('You will be given a random number at the start of the game.There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score.You win the game by matching your total score to the ramdomnumber, you lose the game if your total score goes above the random number.The value of each crystal is hidden from you until you click on it. Each time when the game starts,the game will change the values of each crystal', function(){
+    alert('You will be given a random number at the start of the game.There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score.You win the game by matching your total score to the ramdom number, you lose the game if your total score goes above the random number.The value of each crystal is hidden from you until you click on it. Each time when the game starts,the game will change the values of each crystal', function(){
         console.log("Callback executed");
       });
   }, 500);
